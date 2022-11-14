eb3b72ec754ca8dd15574a3a6b60ae8b016e4601
LapidozZz <112891578+LapidozZz@users.noreply.github.com>
2022-11-14 19:31:06 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,4 +1,5 @@
 import time
+from selenium.webdriver.common.by import By
 
 
 LINK = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
@@ -8,6 +9,7 @@ def test_button_add_to_basket(browser):
     browser.get(LINK)
     time.sleep(30)
     try:
-        btn = browser.find_element_by_css_selector("button.btn-add-to-basket")
+
+        btn = browser.find_element(By.CSS_SELECTOR, "button.btn-add-to-basket")
     except:
         assert False, "Button add to basket is not found!"
